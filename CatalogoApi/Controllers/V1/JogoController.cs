7f90e58ba4cbb8a2b7b1c624234e4fe563eb8c3f
Geovane Silva <geovanesilva.nar@hotmail.com>
2021-09-13 20:48:55 -0300

    Construção da classe Controller e Service

---
+++ CatalogoApi/Controllers/V1/JogoController.cs
@@ -0,0 +1,78 @@
+﻿using CatalogoApi.InputModel;
+using CatalogoApi.Services;
+using CatalogoApi.ViewModel;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CatalogoApi.Controllers.V1
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class JogoController : ControllerBase
+    {
+        private readonly IJogoService _jogoService;
+
+        public JogoController(IJogoService jogoService)
+        {
+            _jogoService = jogoService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<JogoViewModel>>> Obter([FromQuery, Range(1, int.MaxValue)] int pagina = 1, [FromQuery, Range(1, 50)] int quantidade = 5)
+        {
+            var jogos = await _jogoService.Obter(pagina, quantidade);
+
+            if (jogos.Count() == 0)
+                return NoContent();
+            return Ok(jogos);
+        }
+
+        [HttpGet("{idJogo:guid}")]
+        public async Task<ActionResult<JogoViewModel>> Obter([FromRoute] Guid idJogo)
+        {
+            var jogo = await _jogoService.Obter(idJogo);
+
+            if (jogo == null)
+                return NoContent();
+            return Ok(jogo);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<JogoViewModel>> InserirJogo([FromBody] JogoInputmodel JogoInputmodel)
+        {
+            try
+            {
+                var jogo = await _jogoService.Inserir(JogoInputmodel);
+                return Ok(jogo);
+            }
+            catch (Exception ex)
+            {
+                return UnprocessableEntity("Já existe um jogo com este nome para esta produtora");
+            }
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> AtualizarJogo(Guid idJogo, JogoInputmodel Jogo)
+        {
+            return Ok();
+        }
+
+        [HttpPatch("{idJogo:guid}/preco/{preco:double}")]
+        public async Task<ActionResult> AtualizarPreco(Guid idJogo, JogoInputmodel Jogo)
+        {
+            return Ok();
+        }
+
+        [HttpDelete]
+        public async Task<AcceptedResult> ApagarJogo(Guid idJogo)
+        {
+            return Ok();
+        }
+
+    }
+}
---
+++ CatalogoApi/Services/IJogoService.cs
@@ -0,0 +1,19 @@
+﻿using CatalogoApi.InputModel;
+using CatalogoApi.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CatalogoApi.Services
+{
+    public interface IJogoService
+    {
+        Task<List<JogoViewModel>> Obter(int pagina, int quantidade);
+        Task<JogoViewModel> Obter(Guid id);
+        Task<JogoViewModel> Inserir(JogoInputmodel jogo);
+        Task Atualizar(Guid id, JogoInputmodel jogo);
+        Task Atualizar(Guid id, double preco);
+        Task Remover(Guid id);
+    }
+}
